0e9a02d7051aa32c321913a51cdd7557f1984cb1
QLC23 <qlc23@QLC23s-MacBook-Air.local>
2018-02-23 22:39:16 -0700

    Test

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Hallo</Text>
+        <Text>Hallo all!</Text>
       </View>
     );
   }
